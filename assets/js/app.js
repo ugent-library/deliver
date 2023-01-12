87d860d7beecb86343671f7beaea008f6ef269a9
nsteenla <nicolas.steenlant@ugent.be>
2023-01-12 12:07:21 +0100

    confirm delete

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,12 @@
 import BSN from "bootstrap.native/dist/bootstrap-native-v4";
 import bootstrapPopper from './bootstrap_popper.js'
 import formChangeSubmit from './form_change_submit.js'
+import formConfirm from './form_confirm.js'
 
 // initialize everyting
 document.addEventListener('DOMContentLoaded', function () {
     BSN.initCallback()
     bootstrapPopper()
     formChangeSubmit()
+    formConfirm()
 });
--- assets/js/form_change_submit.js
+++ assets/js/form_change_submit.js
@@ -1,14 +1,14 @@
 export default function() {
-    document.querySelectorAll("form.change-submit").forEach(function(el) {
-        el.addEventListener("change", function(evt) {
-            let btn = el.querySelector("button[type='submit']")
+    document.querySelectorAll("form.change-submit").forEach((form) => {
+        form.addEventListener("change", () => {
+            let btn = form.querySelector("button[type='submit']")
             if (btn !== null) {
                 btn.disabled = true
                 let loadingText = btn.dataset.loading || "Loading..."
                 btn.innerHTML = '<span class="spinner-border" role="status" aria-hidden="true"></span> ' + 
                     loadingText
             }
-            el.submit()
+            form.submit()
         });
     });
 }
