6eed9308f4078bee088b52acc2a6c4281246db05
nsteenla <nicolas.steenlant@ugent.be>
2023-01-17 06:55:05 +0100

    clean up upload progress function a bit

--- assets/js/form_upload_progress.js
+++ assets/js/form_upload_progress.js
@@ -1,42 +1,45 @@
 export default function() {
-    document.querySelectorAll("form input[data-upload-progress-target]").forEach(input => {
-        input.addEventListener("change", () => {
+    document.querySelectorAll('form input[data-upload-progress-target]').forEach(input => {
+        input.addEventListener('change', () => {
             let files = Array.from(input.files)
 
-            if (!files.length) {
-                return
-            }
+            if (!files.length) return;
 
             let target = document.getElementById(input.dataset.uploadProgressTarget)
-            let form = input.closest("form")
-            let numUploaded = 0;
+            let form = input.closest('form')
+            let done = 0;
 
             form.style.display = 'none';
 
             files.forEach((file, i) => {
-                let tmpl = document.getElementById("tmpl-upload-progress").content.firstElementChild.cloneNode(true)
+                let tmpl = document.getElementById('tmpl-upload-progress').content.firstElementChild.cloneNode(true)
                 tmpl.querySelector('.upload-name').innerText = file.name
                 target.appendChild(tmpl)
 
                 let formData = new FormData()
                 formData.append('file', file)
                 if (input.dataset.uploadProgressInclude) {
-                    input.dataset.uploadProgressInclude.split(",").forEach((name) => {
+                    input.dataset.uploadProgressInclude.split(',').forEach((name) => {
                         formData.append(name, form.querySelector(`[name="${name}"]`).value)
                     })
                 }
     
-                let ajax = new XMLHttpRequest();
+                let req = new XMLHttpRequest();
 
-                ajax.addEventListener('error', e => {
+                req.addEventListener('error', e => {
                     console.log(e)
+                    done++;
                 }, false);
 
-                ajax.upload.addEventListener('progress', e => {
-                    if (!evt.detail.lengthComputable) {
-                        return
-                    }
-                    let percent = Math.ceil(e.loaded / e.total) * 100
+                req.addEventListener('abort', e => {
+                    tmpl.style.display = 'none';
+                    done++;
+                }, false);
+
+                req.upload.addEventListener('progress', e => {
+                    if (!e.lengthComputable) return;
+
+                    let percent = Math.floor(e.loaded / e.total) * 100
                     tmpl.querySelector('.upload-size').innerText = friendlyBytes(e.loaded)
                     tmpl.querySelector('.upload-percent').innerText = percent
                     let pb = tmpl.querySelector('.progress-bar')
@@ -44,16 +47,16 @@ export default function() {
                     pb.setAttribute('aria-valuenow', percent)
                 }, false);
 
-                ajax.addEventListener('loadend', () => {
+                req.addEventListener('load', () => {
                     tmpl.style.display = 'none';
-                    numUploaded++;
-                    if (numUploaded === files.length) {
+                    done++;
+                    if (done === files.length) {
                         window.location.reload()
                     }
                 }, false);
 
-                ajax.open(form.method, form.action);
-                ajax.send(formData);
+                req.open(form.method, form.action);
+                req.send(formData);
             })
         })
     });
