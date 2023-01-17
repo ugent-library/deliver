5dd285d2e7742f4746eb8584f61d5815bbbbe152
nsteenla <nicolas.steenlant@ugent.be>
2023-01-17 10:26:11 +0100

    confirm dialogs (resolves #11, resolves #12)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,14 @@
 import BSN from "bootstrap.native/dist/bootstrap-native-v4";
 import bootstrapPopper from './bootstrap_popper.js'
-import formChangeSubmit from './form_change_submit.js'
-import formUploadProgress from './form_upload_progress.js'
-import formConfirm from './form_confirm.js'
 import toast from './toast.js'
+import formSubmit from './form_submit.js'
+import formUploadProgress from './form_upload_progress.js'
 
 // initialize everyting
 document.addEventListener('DOMContentLoaded', function () {
     BSN.initCallback()
     bootstrapPopper()
-    formChangeSubmit()
-    formUploadProgress()
-    formConfirm()
     toast()
+    formSubmit()
+    formUploadProgress()
 });
--- assets/js/form_submit.js
+++ assets/js/form_submit.js
@@ -1,4 +1,11 @@
 export default function() {
+    document.querySelectorAll("[data-submit-target]").forEach((btn) => {
+        btn.addEventListener("click", () => {
+            let form = document.querySelector(btn.dataset.submitTarget)
+            form.submit()
+        });
+    });
+
     document.querySelectorAll("form.change-submit").forEach((form) => {
         form.addEventListener("change", () => {
             let btn = form.querySelector("button[type='submit']")
