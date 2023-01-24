1d081ab778933ffc66845ab85c0a5e9f1f947b93
nsteenla <nicolas.steenlant@ugent.be>
2023-01-24 09:35:35 +0100

    middleware docs

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,7 +1,21 @@
+// Pakage middleware contains some generic middlewares and helper functions to
+// make composing middleware more readable.
 package middleware
 
 import "net/http"
 
+// Apply wraps a handler with middlewares. Middleware is applied in the order it
+// is given.
+//
+//	handler = middleware.Apply(handler,
+//	  middleware1,
+//	  middleware2,
+//	  middleware3,
+//	)
+//
+// is equivalant to:
+//
+//	handler = middleware1(middleware2(middleware3(handler)))
 func Apply(h http.Handler, mws ...func(http.Handler) http.Handler) http.Handler {
 	for i := len(mws) - 1; i >= 0; i-- {
 		h = mws[i](h)
@@ -9,6 +23,13 @@ func Apply(h http.Handler, mws ...func(http.Handler) http.Handler) http.Handler 
 	return h
 }
 
+// If only applies a middleware if cond is true.
+//
+//	handler = middleware.Apply(handler,
+//	  middleware1,
+//	  middleware.If(isProduction, middleware2),
+//	  middleware3,
+//	)
 func If(cond bool, mw func(http.Handler) http.Handler) func(http.Handler) http.Handler {
 	if cond {
 		return mw
