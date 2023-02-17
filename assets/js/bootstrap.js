b9cd9b061ebec09b785d120947213952f322d388
Nicolas Franck <nicolas.franck@ugent.be>
2023-02-17 21:46:21 +0100

    cleanup

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -4,7 +4,6 @@ import BSN from 'bootstrap.native/dist/bootstrap-native-v4';
 // Reinitialize Bootstrap Native after HTMX has udated the DOM.
 export default function() {
   htmx.onLoad(function(el) {
-    console.log("loading bootstrap callbacks after htmx load")
     BSN.initCallback()
   });
 }
