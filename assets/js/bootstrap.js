9da0c5ebeadd3c5c1ff6f1721b7d565c5ef4b45c
Nicolas Franck <nicolas.franck@ugent.be>
2023-02-21 12:43:05 +0100

    prevent bootstrap from loaded twice during page load

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -4,6 +4,14 @@ import BSN from 'bootstrap.native/dist/bootstrap-native-v4';
 // Reinitialize Bootstrap Native after HTMX has udated the DOM.
 export default function() {
   htmx.onLoad(function(el) {
+    /*
+     * ugly hack to prevent bootstrap from being initialized twice
+     * context: htmx.onLoad is called once on page load, and then also
+     * for every element fetched. But BSN.initCallback was already
+     * called during page load, and this messes up the close button
+     * of a toast.
+     */
+    if (el.tagName == 'BODY') return
     BSN.initCallback()
   });
 }
