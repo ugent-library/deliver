b1fe9b818e5c324f0ad188e1cb30064ae1c5e0ea
nsteenla <nicolas.steenlant@ugent.be>
2023-03-25 08:49:02 +0100

    remove dependency on templ

--- views/views.go
+++ views/views.go
@@ -1,17 +1,3 @@
 //go:generate go get -u github.com/valyala/quicktemplate/qtc
 //go:generate qtc -dir=views
 package views
-
-import (
-	"context"
-	"io"
-
-	"github.com/a-h/templ"
-)
-
-func raw(s string) templ.Component {
-	return templ.ComponentFunc(func(ctx context.Context, w io.Writer) (err error) {
-		_, err = io.WriteString(w, s)
-		return
-	})
-}
