ecf68a90412a73e2b25af49b04f75ac2ee43ad2f
Koen Verheyen <verheyen.koen@gmail.com>
2023-12-08 14:34:12 +0100

    Clean-up spec improvements

--- cypress/e2e/clean-up.cy.ts
+++ cypress/e2e/clean-up.cy.ts
@@ -1,23 +1,27 @@
 const DEFAULT_SPACE = 'test'
 
 describe('Clean up test folders and files', { redirectionLimit: 1000 }, () => {
+  const SELECTOR = 'table.table tr td:first-of-type a:contains("CYPRESS-")'
+
   it('should clean up all files and folders', () => {
     cy.loginAsSpaceAdmin()
 
     // TODO Remove when issue #99 is resolved
-    Cypress.on('uncaught:exception', (err, runnable) => {
+    Cypress.on('uncaught:exception', () => {
       // returning false here prevents Cypress from failing the test
       return false
     })
 
     deleteAllCypressTestFolders(DEFAULT_SPACE)
+
+    cy.get(SELECTOR).should('not.exist')
   })
 
   function deleteAllCypressTestFolders(space: string) {
     cy.visit(`/spaces/${space}`).then(() => {
-      // Using Cypress.$() direct jQuery selector tool.
+      // Using Cypress.$() direct jQuery selector tool here.
       // Using cy.get() the test would fail if none are left.
-      const links = Cypress.$<HTMLAnchorElement>('table.table tr td:first-of-type a:contains("CYPRESS-")')
+      const links = Cypress.$<HTMLAnchorElement>(SELECTOR)
 
       cy.log(`${links.length} test folder(s) found`)
 
