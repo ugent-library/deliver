da09398ff5c74c86e2e07f142a84375255ee08d4
Koen Verheyen <verheyen.koen@gmail.com>
2024-03-14 14:39:20 +0100

    Removed cached calculations from Pagination model #91

--- models/pagination.go
+++ models/pagination.go
@@ -17,13 +17,11 @@ type Filter struct {
 }
 
 type Pagination struct {
-	offset           int
-	limit            int
-	sort             string
-	total            int
-	filters          []Filter
-	visiblePages     []int
-	paginationString string
+	offset  int
+	limit   int
+	sort    string
+	total   int
+	filters []Filter
 }
 
 func NewPagination(offset int, limit int, sort string, filters ...Filter) *Pagination {
@@ -36,13 +34,11 @@ func NewPagination(offset int, limit int, sort string, filters ...Filter) *Pagin
 	}
 
 	return &Pagination{
-		offset:           offset,
-		limit:            limit,
-		total:            -1,
-		sort:             sort,
-		filters:          filters,
-		visiblePages:     []int{},
-		paginationString: "0",
+		offset:  offset,
+		limit:   limit,
+		total:   -1,
+		sort:    sort,
+		filters: filters,
 	}
 }
 
@@ -60,9 +56,6 @@ func (p *Pagination) Total() int {
 
 func (p *Pagination) SetTotal(total int) {
 	p.total = total
-
-	calculatePaginationString(p)
-	calculateVisiblePages(p)
 }
 
 func (p *Pagination) StartOfPage() int {
@@ -98,7 +91,55 @@ func (p *Pagination) PageOffset(page int) int {
 }
 
 func (p *Pagination) VisiblePages() []int {
-	return p.visiblePages
+	if p.total < 0 {
+		return []int{1}
+	}
+
+	// Initialize map with each possible page number
+	pages := make(map[int]bool, p.NumberOfPages())
+	for i := 1; i <= p.NumberOfPages(); i++ {
+		pages[i] = false
+	}
+
+	// Activate pages 1, current - 1, current, current + 1 and end
+	pages[1] = true
+	pages[p.CurrentPage()-1] = true
+	pages[p.CurrentPage()] = true
+	pages[p.CurrentPage()+1] = true
+	pages[p.NumberOfPages()] = true
+
+	// Delete pages outside boundaries
+	for page := range pages {
+		if page != 1 && (page < 1 || page > p.NumberOfPages()) {
+			delete(pages, page)
+		}
+	}
+
+	// Activate single-gap-pages ("[4] […] [6]" => "[4] [5] [6]")
+	for i := 1; i < p.NumberOfPages(); i++ {
+		if !pages[i] && pages[i-1] && pages[i+1] {
+			pages[i] = true
+		}
+	}
+
+	// Created sorted list of visible pages
+	visiblePages := make([]int, 0, p.NumberOfPages())
+	for page, isVisible := range pages {
+		if isVisible {
+			visiblePages = append(visiblePages, page)
+		}
+	}
+	sort.Ints(visiblePages)
+
+	// Add ellipsis .pager-items in between gaps
+	for i := 0; i < len(visiblePages); i++ {
+		if i >= 1 && visiblePages[i] != visiblePages[i-1]+1 {
+			visiblePages = slices.Insert(visiblePages, i, -1)
+			i++
+		}
+	}
+
+	return visiblePages
 }
 
 func (p *Pagination) Sort() string {
@@ -148,10 +189,6 @@ func (p *Pagination) ToPairs() []string {
 }
 
 func (p *Pagination) PaginationString() string {
-	return p.paginationString
-}
-
-func calculatePaginationString(p *Pagination) {
 	var sb strings.Builder
 
 	if p.total != 0 {
@@ -167,56 +204,5 @@ func calculatePaginationString(p *Pagination) {
 		sb.WriteString("???")
 	}
 
-	p.paginationString = sb.String()
-}
-
-func calculateVisiblePages(p *Pagination) {
-	if p.total < 0 {
-		p.visiblePages = []int{1}
-		return
-	}
-
-	// Initialize map with each possible page number
-	pages := make(map[int]bool, p.NumberOfPages())
-	for i := 1; i <= p.NumberOfPages(); i++ {
-		pages[i] = false
-	}
-
-	// Activate pages 1, current - 1, current, current + 1 and end
-	pages[1] = true
-	pages[p.CurrentPage()-1] = true
-	pages[p.CurrentPage()] = true
-	pages[p.CurrentPage()+1] = true
-	pages[p.NumberOfPages()] = true
-
-	// Delete pages outside boundaries
-	for page := range pages {
-		if page != 1 && (page < 1 || page > p.NumberOfPages()) {
-			delete(pages, page)
-		}
-	}
-
-	// Activate single-gap-pages ("[4] […] [6]" => "[4] [5] [6]")
-	for i := 1; i < p.NumberOfPages(); i++ {
-		if !pages[i] && pages[i-1] && pages[i+1] {
-			pages[i] = true
-		}
-	}
-
-	// Created sorted list of visible pages
-	p.visiblePages = make([]int, 0, p.NumberOfPages())
-	for page, isVisible := range pages {
-		if isVisible {
-			p.visiblePages = append(p.visiblePages, page)
-		}
-	}
-	sort.Ints(p.visiblePages)
-
-	// Add ellipsis .pager-items in between gaps
-	for i := 0; i < len(p.visiblePages); i++ {
-		if i >= 1 && p.visiblePages[i] != p.visiblePages[i-1]+1 {
-			p.visiblePages = slices.Insert(p.visiblePages, i, -1)
-			i++
-		}
-	}
+	return sb.String()
 }
